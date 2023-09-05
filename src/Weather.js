f38ac2b9b9331bd2f4087da4f11632f714725573
krishgupta1 <krishgupta11110@gmail.com>
2023-09-05 21:20:11 +0530

    05-Sep-2023 Designing the Frontend

---
+++ src/Weather.js
@@ -0,0 +1,66 @@
+import React, { useState} from 'react';
+
+const Weather = () => {
+  const [weatherData, setWeatherData] = useState(null);
+  const [city, setCity] = useState('');
+  const [apiKey] = useState('ccf5109e04a1727bc94e962ebc893242'); // Replace with your API key
+  const handleChange = (event) => {
+    setCity(event.target.value);
+  };
+
+  const handleClick = () => {
+    // 👇 "message" stores input field value
+    // setUpdated(city);
+        const fetchData = async () => {
+          try {
+            const response = await fetch(
+              `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+            );
+    
+            if (!response.ok) {
+              throw new Error('Weather data not available');
+            }
+    
+            const data = await response.json();
+            setWeatherData(data);
+          } catch (error) {
+            console.error(error);
+          }
+        };
+    
+        if (city !== '') {
+          fetchData();
+        }
+  };
+  
+
+  return (
+    <div>
+        <div>
+            <h1>Weather App</h1>
+        </div>
+      <div>
+      <input
+        type="text"
+        placeholder="Enter city name"
+        id="city"
+        name="city"
+        onChange={handleChange}
+        value={city}
+        // onChange={(e) => setCity(e.target.value)}
+      />
+      <button onClick={handleClick}>  Search</button>
+      </div>
+      
+      {weatherData && (
+        <div>
+          <h2>{weatherData.name}, {weatherData.sys.country}</h2>
+          <p>Temperature: {weatherData.main.temp}°C</p>
+          <p>Weather: {weatherData.weather[0].description}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Weather;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'nature-img' : "url('/src/components/bg1.jpg')"
+      }
+    },
+  },
+  plugins: [],
+}
